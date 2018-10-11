3abfa42905def1141c0ce9d5473ad0afacba67f2
Manuella Vieira <manuellavr7@gmail.com>
2018-10-11 13:29:29 -0300

    Dividing samples between train and test

--- read_csv.c
+++ read_csv.c
@@ -2,27 +2,28 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define NUM_EXAMPLES  35900
-#define NUM_PIXELS 2305 // Number of pixels + 1 (bias)
+#define TOTAL_SAMPLES 12275
+#define TRAINING_SAMPLES 9795
+#define TEST_SAMPLES 2480
 
 int main(){
 
 
-	char filename[] = "fer2013.csv";
-	int buffer_size = 10000;
+    char filename[] = "fer2013.csv";
+    int buffer_size = 10000, emotion_6 = 0, emotion_4 = 0;
     char *charbuffer, *emotion, *usage, *char_pixels, *temp_pixels;
-    float* pixels, *training, *test;
-    float labels[NUM_EXAMPLES], pixel;
+    float *training, *test;
+    float labels_train[TRAINING_SAMPLES], labels_test[TEST_SAMPLES], pixel;
 
 	FILE* FER_images = fopen(filename, "r");
-    int i = 0;
-    int j = 0;
+    int i_train = 0, i_test = 0, i = 0;
     int offset = 0;
+    int is_training; // 1 == training, 0 == test
 
     charbuffer = (char *)malloc(buffer_size*sizeof(char));
-    pixels = (float *)malloc((2305)*sizeof(float));
     temp_pixels = (char *)malloc(2*sizeof(float));
-    training = (float *)malloc((NUM_EXAMPLES * 2305)*sizeof(float));
+    training = (float *)malloc((TRAINING_SAMPLES * 2305)*sizeof(float));
+    test = (float *)malloc((TEST_SAMPLES * 2305)*sizeof(float));
 
     // Lendo todo o arquivo para treino
 
@@ -31,36 +32,63 @@ int main(){
             char_pixels = strtok(NULL, ",");
             usage = strtok(NULL, ",");
 
+
+            if(strcmp(usage, "Training\n") == 0){
+                is_training = 1;
+            }
+            else
+                is_training = 0;
+
+
             if (strcmp(emotion, "6") == 0 || strcmp(emotion, "4") == 0){
-                if (emotion == "6"){
-                    labels[i] = 1;
-                } else{
-                    labels[i] = 0;
+                if (strcmp(emotion, "6") == 0){
+                    if(is_training == 1){
+                        labels_train[i_train] = 1;
+                        emotion_6++;
+                    }
+                    else{
+                        labels_test[i_test] = 1;
+                    }
+
+                }
+                else{
+                    if(is_training == 1){
+                        labels_train[i_train] = 0;
+                        emotion_4++;
+                    }
+                    else{
+                        labels_test[i_test] = 0;
+                    }
                 }
 
                 temp_pixels = strtok(char_pixels, " ");
-                pixel = atof(temp_pixels);
-                offset = i*2305;
-                training[offset] = pixel/255.0;
 
-                for (j = 1; j < 2304; j++){
-                    temp_pixels = strtok(NULL, " ");
+                for (int j = 0; j < 2304; j++){
+                    pixel = atof(temp_pixels);
 
-                    if(temp_pixels != NULL){
-                        pixel = atof(temp_pixels);
-                        if(strcmp(usage, "Training\n") == 0){
-                            offset = i*2305 + j;
-                            training[offset] = pixel/255.0;
-                        }
+                    if(is_training == 1){
+                        offset = i_train*2305 + j;
+                        training[offset] = pixel/255.0;
                     }
+                    else{
+                        offset = i_test*2305 + j;
+                        test[offset] = pixel/255.0;
+                    }
+
+                    temp_pixels = strtok(NULL, " ");
                 }
-            i++;
+
+                if(is_training == 1){
+                    i_train++;
+                }
+                else{
+                    i_test++;
+                }
           }
     }
 
 
 fclose(FER_images);
-
 
 
 
